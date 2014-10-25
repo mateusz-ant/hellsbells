98c1447492091a47bca0eb6ee11ba9cef4ff6568
Mateusz Antkiewicz <antkiewicz.mateusz@gmail.com>
2014-10-25 12:38:09 +0200

    RandomNumber

--- HellsBells/src/com/aghacks/hellsbells/domain/RandomNumber.java
+++ HellsBells/src/com/aghacks/hellsbells/domain/RandomNumber.java
@@ -1,34 +1,40 @@
 package com.aghacks.hellsbells.domain;
 
-import java.util.Random;
-
 import android.content.Context;
 import android.database.Cursor;
 import android.provider.ContactsContract;
 
+import java.util.Random;
+
 public class RandomNumber {
 
-	public static String[] execute(Context context){
-	Cursor phones = context.getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,null,null, null);
-	String[] name = new String[phones.getColumnCount()];
-	String[] phoneNumber = new String[phones.getColumnCount()];
-	
-	int i=0;
-	while (phones.moveToNext())
-	{
-	  name[i]=phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-	  phoneNumber[i] = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-	  i++;
-	}
-	phones.close();
-	
-	Random r = new Random();
-	final int random= r.nextInt(i);
-	
-	String[] data = new String[2];
-	data[0] = name[random];
-	data[1] = phoneNumber[random];
-	
-	return data;
-	}
+    public static String[] execute(Context context) {
+        String[] data = new String[2];
+        Cursor phones = null;
+        try {
+            phones = context.getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
+
+            int count = phones.getCount();
+
+            if (count == 0) {
+                return data;
+            }
+
+            int rand = new Random().nextInt(count);
+
+            phones.moveToPosition(rand);
+
+            String name = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+            String phoneNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+
+            data[0] = name;
+            data[1] = phoneNumber;
+        } finally {
+            if (phones != null) {
+                phones.close();
+            }
+        }
+
+        return data;
+    }
 }
