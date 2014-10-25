65ff2458de3b77632be4442b8923d63f9eb0a2ff
Mateusz Antkiewicz <antkiewicz.mateusz@gmail.com>
2014-10-25 18:21:33 +0200

    Poprawki (preferences) 2

--- HellsBells/src/com/aghacks/hellsbells/domain/MyTimer.java
+++ HellsBells/src/com/aghacks/hellsbells/domain/MyTimer.java
@@ -9,35 +9,35 @@ import java.util.TimerTask;
 
 public class MyTimer {
 
-	int time;
-	public Timer t;
-	public TimerTask task;
+    int time;
+    public Timer t;
+    public TimerTask task;
 
-	public MyTimer(final Activity activity, final TextView tv) {
+    public MyTimer(final Activity activity, final TextView tv) {
 
-		time = PreferenceManager.getDefaultSharedPreferences(
-				activity.getApplicationContext()).getInt("task_time", 30);
+        time = Integer.parseInt(PreferenceManager.getDefaultSharedPreferences(
+                activity.getApplicationContext()).getString("task_time", "30"));
 
-		t = new Timer();
-		task = new TimerTask() {
+        t = new Timer();
+        task = new TimerTask() {
 
-			@Override
-			public void run() {
-				activity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                activity.runOnUiThread(new Runnable() {
 
-					@Override
-					public void run() {
-						tv.setText(time + "");
-						if (time > 0)
-							time -= 1;
-						else {
-							activity.setResult(Activity.RESULT_CANCELED);
-							activity.finish();
-						}
-					}
-				});
-			}
-		};
-	}
+                    @Override
+                    public void run() {
+                        tv.setText(time + "");
+                        if (time > 0)
+                            time -= 1;
+                        else {
+                            activity.setResult(Activity.RESULT_CANCELED);
+                            activity.finish();
+                        }
+                    }
+                });
+            }
+        };
+    }
 
 }
