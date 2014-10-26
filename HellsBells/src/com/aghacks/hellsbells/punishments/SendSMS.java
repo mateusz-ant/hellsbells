130b45c67ac9c5f4ed25fa09bd9bf4a8750c3da6
dan <dbryla@student.agh.edu.pl>
2014-10-26 12:38:54 +0100

    Added toast about sending SMS.

--- HellsBells/src/com/aghacks/hellsbells/punishments/SendSMS.java
+++ HellsBells/src/com/aghacks/hellsbells/punishments/SendSMS.java
@@ -3,7 +3,10 @@ package com.aghacks.hellsbells.punishments;
 import android.content.Context;
 import android.telephony.SmsManager;
 import android.util.Log;
+import android.widget.Toast;
+
 import com.aghacks.hellsbells.domain.RandomNumber;
+import com.aghacks.hellsbells.task.Messages;
 
 public class SendSMS implements Punishment {
 
@@ -14,10 +17,12 @@ public class SendSMS implements Punishment {
         String name = data[0];
         String phoneNumber = data[1];
         String message = "The message was send to " + name;
-
+        Toast toast = Toast.makeText(context,
+        		"The message was send to " + name, Toast.LENGTH_LONG);
+		toast.show();
         Log.i(this.getClass().getSimpleName(), message);
 
-        //qsendSMS(phoneNumber, "I miss you, call me!");
+        sendSMS(phoneNumber, "I miss you, call me!");
     }
 
     private void sendSMS(String phoneNumber, String message) {
