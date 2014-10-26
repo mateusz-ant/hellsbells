c42231360480ba099825b1a1928b61ea383f16dd
dan <dbryla@student.agh.edu.pl>
2014-10-26 12:02:44 +0100

    Added CALL_PHONE permission.

--- HellsBells/src/com/aghacks/hellsbells/punishments/DownloadFromInternet.java
+++ HellsBells/src/com/aghacks/hellsbells/punishments/DownloadFromInternet.java
@@ -10,36 +10,39 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class DownloadFromInternet implements Punishment {
-    public static final String IMAGE_URL = "http://wallfoy.com/wp-content/uploads/2014/08/landscape_city_night_building_manhatton_ultrahd_4k_wallpaper.jpg";
+	public static final String IMAGE_URL = "http://wallfoy.com/wp-content/uploads/2014/08/landscape_city_night_building_manhatton_ultrahd_4k_wallpaper.jpg";
 
-    @Override
-    public void punish(Context context) {
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    getBitmapFromURL(IMAGE_URL);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-        thread.start();
-    }
+	@Override
+	public void punish(Context context) {
+		Thread thread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					for (int i = 0; i < 10; i++) {
+						getBitmapFromURL(IMAGE_URL);
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		thread.start();
+	}
 
-    private Bitmap getBitmapFromURL(String imageUrl) {
-        try {
-            URL url = new URL(imageUrl);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setDoInput(true);
-            connection.connect();
-            InputStream input = connection.getInputStream();
+	private Bitmap getBitmapFromURL(String imageUrl) {
+		try {
+			URL url = new URL(imageUrl);
+			HttpURLConnection connection = (HttpURLConnection) url
+					.openConnection();
+			connection.setDoInput(true);
+			connection.connect();
+			InputStream input = connection.getInputStream();
 
-            return BitmapFactory.decodeStream(input);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
+			return BitmapFactory.decodeStream(input);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 
 }
